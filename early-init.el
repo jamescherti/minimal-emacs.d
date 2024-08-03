7bd42aeded270f6d1d8710f4926331d9f90c0f96
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-08-03 13:45:11 -0400

    Update comments

--- early-init.el
+++ early-init.el
@@ -4,7 +4,7 @@
 ;; URL: https://github.com/jamescherti/minimal-emacs.d
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: maint
-;; Version: 1
+;; Version: 0.9.9
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;;; Commentary:
