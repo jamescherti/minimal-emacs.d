9eb2a138ec0e4c11ba66f03b4f15c4134f442960
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-03-12 17:18:28 -0400

    Remove MELPA stable

--- early-init.el
+++ early-init.el
@@ -381,12 +381,10 @@ this stage of initialization."
 (setq use-package-always-ensure t)
 (setq use-package-enable-imenu-support t)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (customize-set-variable 'package-archive-priorities '(("gnu"    . 99)
                                                       ("nongnu" . 80)
-                                                      ("melpa-stable" . 70)
                                                       ("melpa"  . 0)))
 
 ;;; Load post-early-init.el
