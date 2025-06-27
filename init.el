66788d1045f5e728346603229f3a66480223bc8d
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-06-27 14:28:46 -0400

    Add dired-auto-revert-buffer

--- init.el
+++ init.el
@@ -396,27 +396,34 @@
 
 ;;; Dired
 
-(setq dired-free-space nil
-      dired-dwim-target t  ; Propose a target for intelligent moving or copying.
-      dired-deletion-confirmer 'y-or-n-p
-      dired-filter-verbose nil
-      dired-recursive-deletes 'top
-      dired-recursive-copies 'always
-      dired-create-destination-dirs 'ask
-      image-dired-thumb-size 150)
+(with-eval-after-load 'dired
+  (setq dired-free-space nil
+        dired-dwim-target t  ; Propose a target for intelligent moving/copying
+        dired-deletion-confirmer 'y-or-n-p
+        dired-filter-verbose nil
+        dired-recursive-deletes 'top
+        dired-recursive-copies 'always
+        dired-create-destination-dirs 'ask)
 
-(setq dired-vc-rename-file t)
+  ;; This is a higher-level predicate that wraps `dired-directory-changed-p'
+  ;; with additional logic. This `dired-buffer-stale-p' predicate handles remote
+  ;; files, wdired, unreadable dirs, and delegates to dired-directory-changed-p
+  ;; for modification checks.
+  (setq dired-auto-revert-buffer 'dired-buffer-stale-p)
 
-;; Disable the prompt about killing the Dired buffer for a deleted directory.
-(setq dired-clean-confirm-killing-deleted-buffers nil)
+  (setq dired-vc-rename-file t)
 
-;; dired-omit-mode
-(setq dired-omit-verbose nil)
-(setq dired-omit-files (concat "\\`[.]\\'"))
+  ;; Disable the prompt about killing the Dired buffer for a deleted directory.
+  (setq dired-clean-confirm-killing-deleted-buffers nil)
+
+  ;; dired-omit-mode
+  (setq dired-omit-verbose nil)
+  (setq dired-omit-files (concat "\\`[.]\\'")))
 
 ;; ls-lisp
-(setq ls-lisp-verbosity nil)
-(setq ls-lisp-dirs-first t)
+(with-eval-after-load 'ls-lisp
+  (setq ls-lisp-verbosity nil)
+  (setq ls-lisp-dirs-first t))
 
 ;;; Ediff
 
