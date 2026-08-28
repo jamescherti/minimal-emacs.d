a7650c6816fd6794ddee603b906911288f94b608
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-28 12:51:27 -0400

    Disable async native compilation warning reports by default
    Set native-comp-async-report-warnings-errors directly to
    minimal-emacs-debug instead of falling back to 'silent. This avoids
    creating background log buffers and remove logging overhead.

--- early-init.el
+++ early-init.el
@@ -215,7 +215,7 @@ pre-early-init.el, and post-early-init.el.")
   (setq features (delq 'native-compile features)))
 
 (setq native-comp-warning-on-missing-source minimal-emacs-debug
-      native-comp-async-report-warnings-errors (or minimal-emacs-debug 'silent))
+      native-comp-async-report-warnings-errors minimal-emacs-debug)
 
 (setq jka-compr-verbose minimal-emacs-debug)
 (setq byte-compile-warnings minimal-emacs-debug
