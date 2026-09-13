0edf4a7fe3861d85eb9f8819db587ed4c6fbc597
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-13 14:49:14 -0400

    Improve performance on Windows by fixing file I/O and process bottlenecks

--- early-init.el
+++ early-init.el
@@ -244,6 +244,12 @@ FILENAME is the full or relative path of the file as a string."
  ;; Disable warnings from the legacy advice API. They aren't useful.
  ad-redefinition-action 'accept)
 
+;; Improve performance on Windows by fixing file I/O and process bottlenecks.
+(when (memq system-type '(windows-nt cygwin ms-dos))
+  (setq w32-pipe-read-delay 0                 ; Faster IPC
+        w32-get-true-file-attributes nil      ; Decrease file IO workload
+        w32-pipe-buffer-size (* 128 1024)))   ; Read more at a time
+
 (when init-file-debug
   (setq message-log-max 16384))
 
